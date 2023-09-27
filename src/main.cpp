f639b3aace390de7a08eb4e0748fb4f533a72522
diegoror <diegorordriguez159@gmail.com>
2023-09-27 04:26:29 -0400

    Modified: ensamblado de programa general

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 // configuracion de pines de salida PWM
-
 const int temPin = 15;
 const int vibPin = 2;
 // configuracion de parametros de PWM
@@ -12,18 +11,29 @@ const int resolucion2 = 9;
 // configuracion de pines de entrada analogica 
 const int temSen = 32;
 const int vibSen = 33;
+const int batSen = 25;
 // Variables de captacion de datos
 int vib = 0;
 int tem = 0;
+int bat = 0;
+int threshold = 40;
 float dutyCycle1 = 0;
 float dutyCycle2 = 0;
+void touchInterrupt(void){
+  Serial.println("tocado");
 
+}
 void setup() {
   ledcSetup(canal0, frecuencia, resolucion);
   ledcAttachPin(temPin, canal0);
   ledcSetup(canal1, frecuencia, resolucion);
   ledcAttachPin(vibPin, canal1);
   analogReadResolution(resolucion2);
+  // Configuracion de sensores touch
+  touchAttachInterrupt(T7, touchInterrupt, threshold);
+  touchAttachInterrupt(T6, touchInterrupt, threshold);
+  touchAttachInterrupt(T5, touchInterrupt, threshold);
+  touchAttachInterrupt(T4, touchInterrupt, threshold);
   Serial.begin(9600);
 }
 
@@ -31,14 +41,21 @@ void loop() {
   
   vib = analogRead(vibSen);
   tem = analogRead(temSen);
+  bat = analogRead(batSen);
+
   dutyCycle1 = (0.499)*vib; 
   ledcWrite(canal0, int(dutyCycle1));
   delay(15);
   dutyCycle2 = (0.499)*tem; 
   ledcWrite(canal1, int(dutyCycle2));
   delay(15);
+  Serial.print("PWM1 = ");
   Serial.println(int(dutyCycle2));
+  Serial.print("PWM2 = ");
   Serial.println(int(dutyCycle1));
+  Serial.print("batt = ");
+  Serial.println(int(0.499*bat));
+
   delay(500);
 }
 
